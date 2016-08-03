13fefd147b7a80a1e72b0bb7fd2162b112f7b157
Ryan Horlacher <horlacherr@gmail.com>
2016-08-03 12:23:45 -0600

    one more

--- example.rb
+++ example.rb
@@ -1,6 +1,11 @@
 puts "What's your name?"
 name = gets.strip
 puts "Hello #{name}"
+
 puts "What's your age?"
 age = gets.to_i
-puts "#{age} you are old!"
+if age > 90
+  puts "#{age} you are old!"
+else
+	puts "You're young"
+
